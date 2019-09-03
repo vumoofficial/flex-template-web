8600eacc46bacfc51343a1adfade55fd74318674
flexmarketplace-joachim <jockee_95@hotmail.com>
2019-09-03 16:10:17 +0200

    New content Help page

--- src/containers/HelpPage/HelpPage.js
+++ src/containers/HelpPage/HelpPage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import config from '../../config';
+import { twitterPageURL } from '../../util/urlHelpers';
 import { StaticPage, TopbarContainer } from '../../containers';
 import {
   LayoutSingleColumn,
@@ -6,7 +8,6 @@ import {
   LayoutWrapperMain,
   LayoutWrapperFooter,
   Footer,
-  NamedLink,
   ExternalLink,
 } from '../../components';
 
@@ -14,14 +15,17 @@ import css from './HelpPage.css';
 import image from './help.jpg';
 
 const HelpPage = () => {
+  const { siteTwitterHandle, siteFacebookPage } = config;
+  const siteTwitterPage = twitterPageURL(siteTwitterHandle);
+
+  // prettier-ignore
   return (
     <StaticPage
-      className={css.root}
       title="Help"
       schema={{
         '@context': 'http://schema.org',
         '@type': 'HelpPage',
-        description: 'Description of this page',
+        description: 'Help',
         name: 'Help page',
       }}
     >
@@ -29,16 +33,60 @@ const HelpPage = () => {
         <LayoutWrapperTopbar>
           <TopbarContainer />
         </LayoutWrapperTopbar>
-        <LayoutWrapperMain>
-          <h1>Some content</h1>
-          <img src={image} alt="My first ice cream." />
-          <div>
-            <NamedLink name="LandingPage">Go to home page</NamedLink> or
-            <ExternalLink href="https://google.com">
-              Go to Google
-            </ExternalLink>
+
+        <LayoutWrapperMain className={css.staticPageWrapper}>
+          <h1 className={css.pageTitle}>Help.</h1>
+          <img className={css.coverImage} src={image} alt="Goer Help" />
+
+          <div className={css.contentWrapper}>
+            <div className={css.contentSide}>
+              <p>Did you know that Finland has 3.2 million saunas - almost one sauna per person!</p>
+            </div>
+
+            <div className={css.contentMain}>
+              <h2>
+                Most of the Finnish saunas are located at the homes of individuals - indeed, most
+                people in Finland live in an apartment with sauna in it. In addition, lots of people
+                have lakeside summer cottages, which also typically come with a separate sauna
+                building near the waterfront.
+              </h2>
+
+              <p>
+                To truly experience a Finnish sauna, you need to look beyond the public saunas, and
+                instead visit a real home or cottage sauna. Saunatime makes this possible for
+                everyone. All our saunas are owned by individuals willing to let tourists and other
+                curious visitors to enter their sacred spaces.
+              </p>
+
+              <h3 className={css.subtitle}>Are you a sauna owner?</h3>
+
+              <p>
+                Saunatime offers you a good way to earn some extra cash! If you're not using your
+                sauna every evening, why not rent it to other people while it's free. And even if
+                you are using your sauna every evening (we understand, it's so good), why not invite
+                other people to join you when the sauna is already warm! A shared sauna experience
+                is often a more fulfilling one.
+              </p>
+
+              <h3 id="contact" className={css.subtitle}>
+                Create your own marketplace like Saunatime
+              </h3>
+              <p>
+                Saunatime is brought to you by the good folks at{' '}
+                <ExternalLink href="http://sharetribe.com">Sharetribe</ExternalLink>. Would you like
+                to create your own marketplace platform a bit like Saunatime? Or perhaps a mobile
+                app? With Sharetribe it's really easy. If you have a marketplace idea in mind, do
+                get in touch!
+              </p>
+              <p>
+                You can also checkout our{' '}
+                <ExternalLink href={siteFacebookPage}>Facebook</ExternalLink> and{' '}
+                <ExternalLink href={siteTwitterPage}>Twitter</ExternalLink>.
+              </p>
+            </div>
           </div>
         </LayoutWrapperMain>
+
         <LayoutWrapperFooter>
           <Footer />
         </LayoutWrapperFooter>
