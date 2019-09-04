13edf23b8f7daa6b016a17a27fffc5c3f8ce22f3
flexmarketplace-joachim <jockee_95@hotmail.com>
2019-09-04 14:18:39 +0200

    Try changing from "smoke" category to "event space"

--- src/marketplace-custom-config.js
+++ src/marketplace-custom-config.js
@@ -38,7 +38,7 @@ export const amenities = [
 ];
 
 export const categories = [
-  { key: 'smoke', label: 'Smoke' },
+  { key: 'event-space', label: 'Event Space' },
   { key: 'electric', label: 'Electric' },
   { key: 'wood', label: 'Wood' },
   { key: 'other', label: 'Other' },
